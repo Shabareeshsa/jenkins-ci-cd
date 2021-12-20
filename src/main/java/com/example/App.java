ce2449ffc40ddf68efd8b11cfe39cb28978350c2
Shabareeshsa <91050610+Shabareeshsa@users.noreply.github.com>
2021-12-20 15:02:59 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,17 @@
 package com.example;
 
+/**
+ * Hello world!
+ */
 public final class App {
     private App() {
     }
-    public static void main(String[] args)
-    {
-        System.out.println("I am Ironman");
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
